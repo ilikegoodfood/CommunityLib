183c370e4f7a7fb7b8cccc41518829583a529746
ilikegoodfood <52924711+ilikegoodfood@users.noreply.github.com>
2022-11-13 17:13:01 +0000

    Successful Filtering Test
    Implemented Exclusive filtering to handle getting data for types that have no subtypes.
    Tested implementation and removed Type casting errors.
    Implemented simple test routine.

--- CommunityLib/Filters.cs
+++ CommunityLib/Filters.cs
@@ -30,54 +30,69 @@ namespace CommunityLib
             return (IDictionary)Activator.CreateInstance (genericDictionaryType);
         }
 
-        public IDictionary CreateDictionary(Type keyType, IList value)
+        public IList GetOrCreateKeyListPair(IDictionary dict, Type key, Type t)
         {
-            Type genericDictionaryType = typeof(Dictionary<,>).MakeGenericType(new Type[] { keyType, value.GetType() });
-            return (IDictionary)Activator.CreateInstance(genericDictionaryType);
-        }
-
-        public IList GetOrCreateKeyListPair(IDictionary dict, Type t)
-        {
-            if (dict.Contains(t))
+            if (dict.Contains(key))
             {
-                return dict[t] as IList;
+                return dict[key] as IList;
             }
             else
             {
                 IList  value = CreateList(t);
-                dict.Add(t, value);
+                dict.Add(key, value);
                 return value;
             }
         }
 
-        public IDictionary CreateSubDictionary(Type t)
+        public IList GetOrCreateKeyListPair(IDictionary dict, object key, Type t)
         {
-            return CreateDictionary(t, CreateList(t));
+            if (dict.Contains(key))
+            {
+                return dict[key] as IList;
+            }
+            else
+            {
+                IList value = CreateList(t);
+                dict.Add(key, value);
+                return value;
+            }
         }
 
-        public bool TryCreateSubDictionary(IDictionary dict, object key, Type t)
+        public IDictionary CreateSubDictionary(Type keyType)
         {
-            if (!dict.Contains(key))
+            return CreateDictionary(keyType, typeof(IList));
+        }
+
+        public bool TryCreateSubDictionary(IDictionary dict, object key, Type keyType)
+        {
+            if (!dict.Contains(key) || dict[key] == null)
             {
-                dict.Add(key, CreateSubDictionary(t));
+                dict.Add(key, CreateSubDictionary(keyType));
                 return true;
             }
             return false;
         }
 
-        public bool TryCreateSubDictionary(IDictionary dict, Type key, Type t)
+        public bool TryCreateSubDictionary(IDictionary dict, Type key, Type keyType)
         {
             if (!dict.Contains(key))
             {
-                dict.Add(key, CreateSubDictionary(t));
+                dict.Add(key, CreateSubDictionary(keyType));
                 return true;
             }
             return false;
         }
 
-        public void CreateAndOrAddToKeyListPair(IDictionary dict, Type t, object value)
+        public void CreateAndOrAddToKeyListPair(IDictionary dict, Type key, Type valueType, object value)
         {
-            GetOrCreateKeyListPair(dict, t).Add(Convert.ChangeType(value, t));
+            GetOrCreateKeyListPair(dict, key, valueType).Add(value);
+            // GetOrCreateKeyListPair(dict, t).Add(Convert.ChangeType(value, t));
+        }
+
+        public void CreateAndOrAddToKeyListPair(IDictionary dict, object key, Type valueType, object value)
+        {
+            GetOrCreateKeyListPair(dict, key, valueType).Add(value);
+            // GetOrCreateKeyListPair(dict, t).Add(Convert.ChangeType(value, t));
         }
 
         public void FilterSocialGroups()
@@ -87,28 +102,35 @@ namespace CommunityLib
 
             foreach (SocialGroup sG in map.socialGroups)
             {
+                //Console.WriteLine("CommunityLib: Filtering social group " + sG.getName() + " of type: " + sG.GetType().Name);
                 // Initialize universal variables
                 Type t = sG.GetType();
 
                 // Conduct one-off operations
+                //Console.WriteLine("CommunityLib: Conducting one-off operations");
                 // Add to Exclusive.
-                CreateAndOrAddToKeyListPair(dictE, t, sG);
+                //Console.WriteLine("CommunityLib: Add to sGByTE");
+                CreateAndOrAddToKeyListPair(dictE, t, t, sG);
 
+                //Console.WriteLine("CommunityLib: Starting While Loop");
                 // Initialize loop onl-variables
                 Type targetT = typeof(SocialGroup);
                 bool flag = false;
 
                 while (flag == false)
                 {
-                    CreateAndOrAddToKeyListPair(dict, t, sG);
+                    //Console.WriteLine("CommunityLib: Add to sGByT as " + t.Name);
+                    CreateAndOrAddToKeyListPair(dict, t, t, sG);
 
                     if (t == targetT)
                     {
                         flag = true;
+                        //Console.WriteLine("CommunityLib: End Loop");
                     }
                     else
                     {
                         t = t.BaseType;
+                        //Console.WriteLine("CommunityLib: Iterate type to " + t.Name);
                     }
                 }
             }
@@ -118,11 +140,10 @@ namespace CommunityLib
         {
             foreach (Unit u in map.units)
             {
+                //Console.WriteLine("CommunityLib: Filtering unit " + u.getName() + " of type: " + u.GetType().Name);
                 // Initialize universal variables
                 Type t = u.GetType();
                 bool commandable = u.isCommandable();
-                IList valueL;
-                IDictionary valueD;
 
                 // Dictionaries being operated on at all level.
                 IDictionary uBySG = cache.unitsBySocialGroup;
@@ -134,26 +155,32 @@ namespace CommunityLib
                 IDictionary cUBySGByT = cache.commandableUnitsBySocialGroupByType;
                 IDictionary cUBySGByTE = cache.commandableUnitsBySocialGroupByType;
 
+                //Console.WriteLine("CommunityLib: Conducting one-off operations");
                 // Conduct one-off operations
                 // Add units to unitsBySocialGroups
-                CreateAndOrAddToKeyListPair(uBySG, t, u);
-                CreateAndOrAddToKeyListPair(uBySGE, t, u);
+                //Console.WriteLine("CommunityLib: Add to uBySG and uBySGE");
+                CreateAndOrAddToKeyListPair(uBySG, u.society, typeof(Unit), u);
+                CreateAndOrAddToKeyListPair(uBySGE, u.society, typeof(Unit), u);
                 // GetOrCreate subdictionary for social group in unitsBySocialGroupsByType
-                TryCreateSubDictionary(uBySGByT, u.society, t);
-                TryCreateSubDictionary(uBySGByTE, u.society, t);
-                CreateAndOrAddToKeyListPair(uBySGByTE[u.society] as IDictionary, t, u);
+                //Console.WriteLine("CommunityLib: Establishing <SocialGroup, SubDictionary<" + typeof(Type).Name + ", List<" + t.Name + ">> Pairs");
+                TryCreateSubDictionary(uBySGByT, u.society, typeof(Type));
+                TryCreateSubDictionary(uBySGByTE, u.society, typeof(Type));
+                CreateAndOrAddToKeyListPair(uBySGByTE[u.society] as IDictionary, t, t, u);
 
                 if (commandable)
                 {
                     // Add units to commandableUnitsBySocialGroups
-                    CreateAndOrAddToKeyListPair(cUBySG, t, u);
-                    CreateAndOrAddToKeyListPair(cUBySGE, t, u);
+                    //Console.WriteLine("CommunityLib: Add to cUBySG");
+                    CreateAndOrAddToKeyListPair(cUBySG, u.society, typeof(Unit), u);
+                    CreateAndOrAddToKeyListPair(cUBySGE, u.society, typeof(Unit), u);
                     // GetOrCreate subdictionary for social group in commandableUnitsBySocialGroupsByType
-                    TryCreateSubDictionary(cUBySGByT, u.society, t);
-                    TryCreateSubDictionary(cUBySGByTE, u.society, t);
-                    CreateAndOrAddToKeyListPair(cUBySGByTE[u.society] as IDictionary, t, u);
+                    //Console.WriteLine("CommunityLib: Establishing Key SG, SubDictionary Pairs for commandable");
+                    TryCreateSubDictionary(cUBySGByT, u.society, typeof(Type));
+                    TryCreateSubDictionary(cUBySGByTE, u.society, typeof(Type));
+                    CreateAndOrAddToKeyListPair(cUBySGByTE[u.society] as IDictionary, t, t, u);
                 }
 
+                //Console.WriteLine("CommunityLib: Starting While Loop");
                 // Initialize loop-only variables
                 Type targetT = typeof(Unit);
                 bool flag = false;
@@ -163,15 +190,19 @@ namespace CommunityLib
                 {
                     // The subdictionaries used in this loop were checked for, and if neccesary created, earlier in this method.
                     // Add units to unitsByType
-                    CreateAndOrAddToKeyListPair(cache.unitsByType, t, u);
+                    //Console.WriteLine("CommunityLib: Add to uByT");
+                    CreateAndOrAddToKeyListPair(cache.unitsByType, t, t, u);
                     // Add units to unitsBySocialGroupByType subdictionary, unitsByType
-                    CreateAndOrAddToKeyListPair(uBySGByT[u.society] as IDictionary, t, u);
+                    //Console.WriteLine("CommunityLib: Add to uBySGByT[" + u.society.name + "]");
+                    CreateAndOrAddToKeyListPair(uBySGByT[u.society] as IDictionary, t, t, u);
                     if (commandable)
                     {
                         // Add units to commandableUnitsByType
-                        CreateAndOrAddToKeyListPair(cache.commandableUnitsByType, t, u);
+                        //Console.WriteLine("CommunityLib: Add to cUByT");
+                        CreateAndOrAddToKeyListPair(cache.commandableUnitsByType, t, t, u);
                         // Add units to unitsBySocialGroupByType subdictionary, unitsByType
-                        CreateAndOrAddToKeyListPair(cUBySGByT[u.society] as IDictionary, t, u);
+                        //Console.WriteLine("CommunityLib: Add to cUBySGByT[" + u.society.name + "]");
+                        CreateAndOrAddToKeyListPair(cUBySGByT[u.society] as IDictionary, t, t, u);
                     }
 
                     // Check if Type t is targetT. End loop if it is.
@@ -179,10 +210,12 @@ namespace CommunityLib
                     if (t == targetT)
                     {
                         flag = true;
+                        //Console.WriteLine("CommunityLib: End Loop");
                     }
                     else
                     {
                         t = t.BaseType;
+                        //Console.WriteLine("CommunityLib: Iterate type to " + t.Name);
                     }
                 }
             }
--- CommunityLib/ModCore.cs
+++ CommunityLib/ModCore.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Assets.Code;
+using UnityEngine.Diagnostics;
 
 namespace CommunityLib
 {
@@ -34,6 +35,57 @@ namespace CommunityLib
             // Begin Filtering Process.
             filters.FilterSocialGroups();
             filters.FilterUnits();
+            //testRoutine(map);
+        }
+
+        private void testRoutine(Map map)
+        {
+            SG_Orc randOrcSG = null;
+            Society randSociety = null;
+            HolyOrder randHumanHolyOrder= null;
+            HolyOrder_Witches randWitchHolyOrder = null;
+            UA randCommandableAgent = null;
+            int rand;
+
+            if (cache.socialGroupsByType.ContainsKey(typeof(SG_Orc)))
+            {
+                List<SG_Orc> orcSocieties = cache.socialGroupsByType[typeof(SG_Orc)] as List<SG_Orc>;
+                rand = Eleven.random.Next(orcSocieties.Count());
+                randOrcSG = orcSocieties[rand];
+            }
+
+            if (cache.socialGroupsByType.ContainsKey(typeof(Society)))
+            {
+                List<Society> societies = cache.socialGroupsByType[typeof(Society)] as List<Society>;
+                rand = Eleven.random.Next(societies.Count());
+                randSociety = societies[rand];
+            }
+
+            if (cache.socialGroupsByTypeExclusive.ContainsKey(typeof(HolyOrder)))
+            {
+                List<HolyOrder> humanHolyOrders = cache.socialGroupsByTypeExclusive[typeof(HolyOrder)] as List<HolyOrder>;
+                rand = Eleven.random.Next(humanHolyOrders.Count());
+                randHumanHolyOrder = humanHolyOrders[rand];
+            }
+
+            if (cache.socialGroupsByType.ContainsKey(typeof(HolyOrder_Witches)))
+            {
+                List<HolyOrder_Witches> witchHolyOrders = cache.socialGroupsByType[typeof(HolyOrder_Witches)] as List<HolyOrder_Witches>;
+                rand = Eleven.random.Next(witchHolyOrders.Count());
+                randWitchHolyOrder = witchHolyOrders[rand];
+            }
+
+            if (cache.commandableUnitsByType.ContainsKey(typeof(UA)))
+            {
+                List<UA> commandableAgents = cache.commandableUnitsByType[typeof(UA)] as List<UA>;
+                rand = Eleven.random.Next(commandableAgents.Count());
+                randCommandableAgent = commandableAgents[rand];
+            }
+
+            if (randOrcSG != null && randSociety != null && randHumanHolyOrder != null && randWitchHolyOrder != null && randCommandableAgent != null)
+            {
+                map.addUnifiedMessage(randHumanHolyOrder, randWitchHolyOrder, "Cache Testing", "This is a batch of tests for the cacher. It retrieved the following items at random: Random Orc Social Group: " + randOrcSG.getName() + ". Random society: " + randSociety.getName() + ". Random Human Holy Order: " + randHumanHolyOrder.getName() + ". Random Wicth Holy Order: " + randWitchHolyOrder.getName() + ". Random Commandable Agent: " + randCommandableAgent.getName(), "CommunityLib Test");
+            }
         }
 
         public Cache GetCache()
