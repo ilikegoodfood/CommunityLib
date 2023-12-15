df8e53d28c7739a1f1e2308ee171b3dabd8f2b96
ilikegoodfood <ilikegoodfood@googlemail.com>
2023-12-15 16:37:58 +0000

    Update Task_GoToPerformChallengeAtLocation.cs

--- CommunityLib/Task_GoToPerformChallengeAtLocation.cs
+++ CommunityLib/Task_GoToPerformChallengeAtLocation.cs
@@ -136,11 +136,12 @@ namespace CommunityLib
                 }
                 else
                 {
-                    unit.task = null;
                     if (unit.isCommandable() || target.isWatched)
                     {
                         unit.map.addUnifiedMessage(unit, null, "Unit Arrives", unit.getName() + " has reached " + unit.location.getName(true) + " but could not perform challenge " + challenge.getName() + " because the requirements to start this challenge have not yet been met.", UnifiedMessage.messageType.UNIT_ARRIVES, false);
                     }
+
+                    unit.task = null;
                 }
 
             }
