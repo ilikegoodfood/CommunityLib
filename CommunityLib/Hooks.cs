2650b5073309188a99b3021744d8c4dfcc5b918b
ilikegoodfood <ilikegoodfood@googlemail.com>
2024-04-19 21:14:37 +0100

    Fixed `BattleAgents_step_Transpiler`
    - `BattleAgents_step_Transpiler` now accounts for the three possible return states of the intercept hook.
    - intercept hook now returns both a bool, and outs a bool (if the battle is complete).

--- CommunityLib/Hooks.cs
+++ CommunityLib/Hooks.cs
@@ -356,7 +356,7 @@ namespace CommunityLib
         /// </summary>
         /// <param name="battle"></param>
         /// <returns></returns>
-        public virtual bool interceptAgentBattleStep(PopupBattleAgent popupBattle, BattleAgents battle)
+        public virtual bool interceptAgentBattleStep(PopupBattleAgent popupBattle, BattleAgents battle, out bool battleOver)
         {
             battleOver = false;
             return false;
