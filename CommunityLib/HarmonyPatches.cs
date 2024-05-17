8621d0636b6a3d534f4b265e69c11d7c9c6deb3c
ilikegoodfood <ilikegoodfood@googlemail.com>
2024-05-17 13:55:58 +0100

    Typos and Logging
    - Fixed Typos in `World.Log` outputs for trade routes.
    - Increase logging outputs for trade routes.
    - Minor performance boost for VANILLA trade delegate.

--- CommunityLib/HarmonyPatches.cs
+++ CommunityLib/HarmonyPatches.cs
@@ -2886,7 +2886,7 @@ namespace CommunityLib
                 endpointsDisconnected.Remove(endpointConnected);
             }
 
-            World.log($"CommunityLib: Found {endpointsDisconnected.Count} endpoints for trade routes that aree not connected to a trade route. Createing 1 new trade route for each of them...");
+            World.log($"CommunityLib: Found {endpointsDisconnected.Count} endpoints for trade routes that are not connected to a trade route.");
             while (endpointsDisconnected.Count > 0)
             {
                 List<Location> newlyConnectedEndpoints = new List<Location>();
@@ -2896,6 +2896,7 @@ namespace CommunityLib
                     if (routePath == null || routePath.Length < 2)
                     {
                         newlyConnectedEndpoints.Add(endpointDisconnected);
+                        World.log($"CommunityLib: Trade Route created from {endpointDisconnected.getName()} failed to link to anywhere. Skipping enpoint...");
                         break;
                     }
                     else
@@ -2903,6 +2904,7 @@ namespace CommunityLib
                         newlyConnectedEndpoints.Add(endpointDisconnected);
                         newlyConnectedEndpoints.Add(routePath[routePath.Length - 1]);
                         tradeManager.routes.Add(new TradeRoute(routePath.ToList()));
+                        World.log($"CommunityLib: Trade Route created from {endpointDisconnected.getName()} to {routePath[routePath.Length - 1].getName()}");
                         break;
                     }
                 }
