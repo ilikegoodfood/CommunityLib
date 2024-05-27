fa4e2223535f645b2fee0d36cde5d666afe0232f
ilikegoodfood <ilikegoodfood@googlemail.com>
2024-05-27 19:10:52 +0100

    Trimmed Unused Label

--- CommunityLib/HarmonyPatches.cs
+++ CommunityLib/HarmonyPatches.cs
@@ -3541,7 +3541,6 @@ namespace CommunityLib
 
             Label falseLabel = ilg.DefineLabel();
             Label allowMultiSkipLabel = ilg.DefineLabel();
-            Label challengeFoundSkipLabel = ilg.DefineLabel();
 
             int targetIndex = 1;
             for (int i = 0; i < instructionList.Count; i++)
