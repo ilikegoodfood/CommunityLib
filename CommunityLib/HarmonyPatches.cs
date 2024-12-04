2465358cd95e58415425489525b86180bc1df4d0
ilikegoodfood <ilikegoodfood@googlemail.com>
2024-12-04 19:26:14 +0000

    Fixed Orc Capital wondering

--- CommunityLib/HarmonyPatches.cs
+++ CommunityLib/HarmonyPatches.cs
@@ -4358,6 +4358,11 @@ namespace CommunityLib
                         }
                     }
 
+            if (orcs.capital != -1 && (orcs.map.locations[orcs.capital].soc != orcs || !(orcs.map.locations[orcs.capital].settlement is Set_OrcCamp)))
+            {
+                orcs.capital = -1;
+            }
+
                     foreach (Location neighbour in location.getNeighbours())
                     {
                         if (orcs.canSettle(neighbour))
