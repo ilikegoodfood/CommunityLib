4e9ae585860a8e547880074670042e8c9a770147
ilikegoodfood <ilikegoodfood@googlemail.com>
2025-01-15 12:28:18 +0000

    `TestItem` console command
    - Added `I_TestItem_Blank`,  which has the standard back icon as it's foreground icon.
    - Made Community Library console commands case insensitive.
    - Merged "noDeath" console command into "testItem [item]" console command
    - Implemented "testItem [item]" console command  that gives the selected person the specified test item.
    - - Options are "blank", and "nodeath"

--- CommunityLib/ConsoleCommands.cs
+++ CommunityLib/ConsoleCommands.cs
@@ -11,7 +11,8 @@ namespace CommunityLib
     {
         public static void parseCommand(string command, Map map)
         {
-            string[] commandComps = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            string lowerCaseCommnand = command.ToLower();
+            string[] commandComps = lowerCaseCommnand.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
             if (commandComps.Length > 0)
             {
@@ -22,7 +23,7 @@ namespace CommunityLib
                         cheat_LinkLocations(map, val1, val2);
                     }
                 }
-                else if (commandComps[0] == "influenceElder")
+                else if (commandComps[0] == "influenceelder")
                 {
                     if (commandComps.Length == 1)
                     {
@@ -33,7 +34,7 @@ namespace CommunityLib
                         cheat_InfluenceHolyOrder(val, true);
                     }
                 }
-                else if (commandComps[0] == "influenceHuman")
+                else if (commandComps[0] == "influencehuman")
                 {
                     if (commandComps.Length == 1)
                     {
@@ -55,62 +56,7 @@ namespace CommunityLib
                         cheat_Gold(val3);
                     }
                 }
-                else if (commandComps[0] == "noDeath")
-                {
-                    Unit selectedUnit = GraphicalMap.selectedUnit;
-                    if (selectedUnit != null)
-                    {
-                        if (selectedUnit.person != null)
-                        {
-                            int index = -1;
-                            for (int i = 0; i < selectedUnit.person.items.Length; i++)
-                            {
-                                if (selectedUnit.person.items[i] == null)
-                                {
-                                    index = i;
-                                    break;
-                                }
-                            }
-
-                            if (index != -1)
-                            {
-                                selectedUnit.person.items[index] = new I_Test_DeathSave(map);
-                            }
-                            else
-                            {
-                                selectedUnit.person.items[0] = new I_Test_DeathSave(map);
-                            }
-                        }
-                    }
-
-                    Hex selectedHex = GraphicalMap.selectedHex;
-                    if (selectedHex != null)
-                    {
-                        Location selectedLocation = selectedHex.location;
-                        if (selectedLocation != null && selectedLocation.settlement is SettlementHuman settlementHuman && settlementHuman.ruler != null)
-                        {
-                            int index = -1;
-                            for (int i = 0; i < settlementHuman.ruler.items.Length; i++)
-                            {
-                                if (settlementHuman.ruler.items[i] == null)
-                                {
-                                    index = i;
-                                    break;
-                                }
-                            }
-
-                            if (index != -1)
-                            {
-                                settlementHuman.ruler.items[index] = new I_Test_DeathSave(map);
-                            }
-                            else
-                            {
-                                settlementHuman.ruler.items[0] = new I_Test_DeathSave(map);
-                            }
-                        }
-                    }
-                }
-                else if (commandComps[0] == "blazingSun")
+                else if (commandComps[0] == "blazingsun")
                 {
                     foreach(Hex[] array in map.grid)
                     {
@@ -156,6 +102,78 @@ namespace CommunityLib
                         if (!person.isDead && person.society is Society society && !society.isDark() && !society.isDarkEmpire && !society.isOphanimControlled)
                         {
                             person.shadow = 0.0f;
+                        }
+                    }
+                }
+                else if (commandComps[0] == "testitem")
+                {
+                    Item item = new I_TestItem_Blank(map);
+                    if (commandComps.Length > 1)
+                    {
+                        if (commandComps[1] == "blank")
+                        {
+                            item = new I_TestItem_Blank(map);
+                        }
+                        else if (commandComps[1] == "nodeath")
+                        {
+                            item = new I_Test_DeathSave(map);
+                        }
+                    }
+
+                    Unit selectedUnit = GraphicalMap.selectedUnit;
+                    if (selectedUnit != null)
+                    {
+                        if (selectedUnit.person != null)
+                        {
+                            int index = -1;
+                            for (int i = 0; i < selectedUnit.person.items.Length; i++)
+                            {
+                                if (selectedUnit.person.items[i] == null)
+                                {
+                                    index = i;
+                                    break;
+                                }
+                            }
+
+                            if (index != -1)
+                            {
+                                selectedUnit.person.items[index] = item;
+                            }
+                            else if (selectedUnit.isCommandable())
+                            {
+                                selectedUnit.person.gainItem(item);
+                            }
+                            else
+                            {
+                                selectedUnit.person.items[0] = item;
+                            }
+                        }
+                    }
+
+                    Hex selectedHex = GraphicalMap.selectedHex;
+                    if (selectedHex != null)
+                    {
+                        Location selectedLocation = selectedHex.location;
+                        if (selectedLocation != null && selectedLocation.settlement is SettlementHuman settlementHuman && settlementHuman.ruler != null)
+                        {
+                            int index = -1;
+                            for (int i = 0; i < settlementHuman.ruler.items.Length; i++)
+                            {
+                                if (settlementHuman.ruler.items[i] == null)
+                                {
+                                    index = i;
+                                    break;
+                                }
+                            }
+
+                            if (index != -1)
+                            {
+                                settlementHuman.ruler.items[index] = item;
+                            }
+                            else
+                            {
+                                settlementHuman.ruler.items[0] = item;
+                            }
                         }
                     }
                 }
---
+++ CommunityLib/I_TestItem_Blank.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CommunityLib
+{
+    public class I_TestItem_Blank : Item
+    {
+        public I_TestItem_Blank(Map map)
+            : base(map)
+        {
+
+        }
+
+        public override string getName()
+        {
+            return "Blank Test Item";
+        }
+
+        public override string getShortDesc()
+        {
+            return "A test item used for devlopement. It  has not in-game uses.";
+        }
+
+        public override Sprite getIconFore()
+        {
+            return map.world.iconStore.standardBack;
+        }
+    }
+}
