e7358d6c0cac2df655561986d6526fcb4eefaad7
ilikegoodfood <ilikegoodfood@googlemail.com>
2026-02-02 16:17:54 +0000

    Cache Accessability
    - Items Carried map mask now shows cache items as inaccessible if an agent is in the process of accessing the cache.

--- CommunityLib/ModCore.cs
+++ CommunityLib/ModCore.cs
@@ -827,6 +827,7 @@ namespace CommunityLib
                 }
 
                 Item targetItem = hex.map.world.ui.uiScrollables.scrollable_threats.targetItem;
+                bool hasInaccessableItem = false;
                 foreach (Property property in hex.location.properties)
                 {
                     if (!(property is Pr_ItemCache cache))
@@ -838,12 +839,26 @@ namespace CommunityLib
                     {
                         if (cache.items.Any(i => i != null && i.getName() == targetItem.getName()))
                         {
-                            return Color.clear;
+                            if (!hex.location.units.Any(u => u is UA ua && ua.task is Task_PerformChallenge pChallenge && pChallenge.challenge == cache.challenges.FirstOrDefault(ch => ch is Ch_AccessCache)))
+                            {
+                                return Color.clear;
+                            }
+                            else
+                            {
+                                hasInaccessableItem = true;
+                            }
                         }
                     }
                     else if (cache.items.Any(i => i != null))
                     {
-                        return Color.clear;
+                        if (!hex.location.units.Any(u => u is UA ua && ua.task is Task_PerformChallenge pChallenge && pChallenge.challenge == cache.challenges.FirstOrDefault(ch => ch is Ch_AccessCache)))
+                        {
+                            return Color.clear;
+                        }
+                        else
+                        {
+                            hasInaccessableItem = true;
+                        }
                     }
                 }
 
@@ -864,7 +879,7 @@ namespace CommunityLib
                                 }
                                 else
                                 {
-                                    return new Color(1.0f, 0.9f, 0f, 0.9f);
+                                    hasInaccessableItem = true;
                                 }
                             }
                             else
@@ -884,7 +899,7 @@ namespace CommunityLib
                             }
                             else
                             {
-                                return new Color(1.0f, 0.9f, 0f, 0.9f);
+                                hasInaccessableItem = true;
                             }
                         }
                         else
@@ -892,6 +907,11 @@ namespace CommunityLib
                             return Color.clear;
                         }
                     }
+                }
+
+                if (hasInaccessableItem)
+                {
+                    return new Color(1.0f, 0.9f, 0f, 0.9f);
                 }
 
                 if (opt_transparentMaskBackgrounds)
