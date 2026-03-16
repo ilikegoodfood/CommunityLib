2a35c8f3e8bce0f4d256efdb72f1aeb9f516a137
ilikegoodfood <ilikegoodfood@googlemail.com>
2026-03-16 23:35:49 +0000

    Update Pathfinding.cs
    - Fixed incorrect destinationPaths instantiation.

--- CommunityLib/Pathfinding.cs
+++ CommunityLib/Pathfinding.cs
@@ -488,7 +488,7 @@ namespace CommunityLib
 
                     if (currentLocation.soc == sg)
                     {
-                        List<Location[]> destinationPaths = new List<Location[]>();
+                        List<Location[]> destinationPaths = new List<Location[]> { pair.Value };
                         ValuePriorityPair<Location[], double> potentialPair;
                         while (paths.Count > 0)
                         {
@@ -717,7 +717,7 @@ namespace CommunityLib
                     }
                     if (isValid)
                     {
-                        List<Location[]> destinationPaths = new List<Location[]>();
+                        List<Location[]> destinationPaths = new List<Location[]> { pair.Value };
                         ValuePriorityPair<Location[], double> potentialPair;
                         while (paths.Count > 0)
                         {
