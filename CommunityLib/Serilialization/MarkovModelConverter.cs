ef4931b462ed2be556169148b0304e7f195e0621
ilikegoodfood <ilikegoodfood@googlemail.com>
2026-05-10 14:11:42 +0100

    Serialization Fix
    - Implemented fsObjectProcessor that tracks types in a stack, allowing for stack tracing.
    - Implemented Converters for n-dimensional arrays and MarkovModels to prevent save failures.
    - - These converters prevent serialization, they do not serialize them.
    - - Data stored in n-dimensional arrays will be lost when saving, and replaced with null upon loading.
    - Implemented harmony transpiler patch that injects fsSerializer components as needed, and logs the type stack on an exception being caught.

---
+++ CommunityLib/Serilialization/MarkovModelConverter.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Code;
+using FullSerializer;
+using System;
+
+namespace CommunityLib.Serialization
+{
+    public class MarkovModelConverter : fsConverter
+    {
+        public override bool CanProcess(Type type)
+        {
+            return typeof(MarkovModel).IsAssignableFrom(type);
+        }
+
+        public override fsResult TryDeserialize(fsData data, ref object instance, Type storageType)
+        {
+            return fsResult.Success;
+        }
+
+        public override fsResult TrySerialize(object instance, out fsData serialized, Type storageType)
+        {
+            Console.WriteLine($"CommunityLib: Prevented serialization of MarkovModel instance.");
+
+            serialized = fsData.Null;
+            return fsResult.Success;
+        }
+    }
+}
