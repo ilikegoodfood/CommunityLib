d6db49a60dfd77986c756658ef28891987a83335
ilikegoodfood <ilikegoodfood@googlemail.com>
2026-06-23 12:05:14 +0100

    First Daughter Fix
    - Added new subtype of `Pr_EventCustom`; "Pr_EventCustom_SurvivesRuin".
    - - survives ruin returns true from this property.
    - Added new "ADD_MODIFIER_SURVIVES_RUIN" event outcome.
    - - This functions identically to "ADD_MODIFIER", except that it spawns and merges with `Pr_EventCustom_SurvvesRuin` instances, instead of `Pr_EventCustom`.
    - Overrode "location_seerDaughter.json" to use the "ADD_MODIFIER_SURVIVES_RUIN" event outcome.

---
+++ CommunityLib/Pr_EventCustom_SurvivesRuin.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Code;
+using UnityEngine;
+
+namespace CommunityLib
+{
+    public class Pr_EventCustom_SurvivesRuin : Pr_EventCustom
+    {
+        public Pr_EventCustom_SurvivesRuin(Location loc)
+            : base(loc)
+        {
+
+        }
+
+        public override bool survivesRuin()
+        {
+            return true;
+        }
+    }
+}
